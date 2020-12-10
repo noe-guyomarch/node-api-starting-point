6f6574ef855c6328b70b61cbdc9b6460676748cb
noé-guyomarch <49200628+noe-guyomarch@users.noreply.github.com>
2020-12-10 10:59:37 +0100

    1rst maj

--- src/routes/barathons.ts
+++ src/routes/barathons.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import mongoose from 'mongoose';
 import getModel from '../db/models';
 
 const router = Router();
@@ -10,6 +11,24 @@ router.get('/', async (req: Request, res: Response): Promise<void> => {
     // find sans paramètre renvoie l'ensemble des documents de la collection
     const barathons = await barathonModel.find();
     res.json(barathons);
+});
+
+router.post('/', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const barathonModel = getModel ('barathon');
+        await barathonModel.validate(req.body);
+        const barathon = new barathonModel(req.body);
+        const insertedBarathon = await barathon.save();
+        res.json(insertedBarathon);    
+    } catch (err) {
+        if (err instanceof mongoose.Error.ValidationError) {
+            res.status(408);
+            res.json({ error: true,  message: 'Bad request'});     
+        }else{
+            res.status(508);
+            res.json({ error: true,  message: 'Server error'});                 
+        }
+    }
 });
 
 export default router;
--- src/routes/pubs.ts
+++ src/routes/pubs.ts
@@ -1,16 +1,30 @@
 // src/routes/pubs.ts
-import { Router, Request, Response } from 'express';
-import getModel from '../db/models';
+
+
+import { Request, Response, Router } from 'express';
+import getModel from "../db/models";
 
 const router = Router();
 
-// renvoie un tableau avec tous les pubs
 router.get('/', async (req: Request, res: Response): Promise<void> => {
     const pubModel = getModel('pub');
-
-    // find sans paramètre renvoie l'ensemble des documents de la collection
     const pubs = await pubModel.find();
     res.json(pubs);
+});
+
+router.get('/:id', async (req: Request, res: Response): Promise<void> =>  {
+    try{
+        const pubModel = getModel('pub');
+        const pub = await pubModel.findById(req.params.id);
+        if(!pub){
+            throw(new Error('Pub not fount'));
+        }
+        res.json(pub);
+    }catch(err){
+        res.status(404);
+        res.json({ error: true, message: `Pub with id ${req.params.id} not found` });
+    }
+
 });
 
 export default router;
